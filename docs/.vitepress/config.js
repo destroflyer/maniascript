02b7a7761bf096526e7e0b6116ecc547eecc1bb9
carlkuesters <carl.kuesters@youengineering.com>
2023-01-19 23:48:00 +0100

    Initial setup, WIP.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -53,6 +53,7 @@ export default {
             { text: "Network", link: "/advanced/network.md" },
             { text: "Persistence", link: "/advanced/persistence.md" },
             { text: "Translations", link: "/advanced/translations.md" },
+            { text: "Metadata", link: "/advanced/metadata.md" },
             { text: "Reference generation", link: "/advanced/reference_generation.md" },
           ],
         },
